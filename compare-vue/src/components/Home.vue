e24d92a6c47159ee7eb39b752bbd056fa488dfcf
Gabriel Gambarra <gabriel.gambarra@itriad.org.br>
2020-01-14 18:03:18 -0400

    Finish first part of Home component

--- compare-vue/src/components/Home.vue
+++ compare-vue/src/components/Home.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="home">Home</div>
+  <div class="home">
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">{{ heading }}</h1>
+          <div class="is-two-thirds column is-paddingless">
+            <h2 class="subtitle">{{ subheading }}</h2>
+          </div>
+          <a href="" class="button is-large is-primary" id="learn">Learn more</a>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -7,8 +19,9 @@ export default {
   name: "home",
   data() {
     return {
-      msg: "Welcome to Your Vue.js App"
-    };
+      heading: 'Soaring to new heights',
+      subheading: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    }
   }
 };
 </script>
@@ -17,4 +30,19 @@ export default {
 <style lang="sass" scoped>
 @import '../mq'
 
+.hero
+  background: url('../assets/clouds.jpg')
+  background-size: cover
+
+  .title
+    +mobile
+      font-weight: bold
+    +tablet
+      font-size: 2.5rem
+    +desktop
+      font-size: 4rem
+      margin-top: 2rem
+
+h2
+  margin: 1.5rem 0 2rem 0 !important
 </style>
