0db484d67ac4524b8d00a885b03b1d094c9d111c
Gabriel Gambarra <gabriel.gambarra@itriad.org.br>
2020-01-14 11:08:20 -0400

    Add bulma framework

---
+++ compare-vue/src/components/Faq.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="faq">FAQ</div>
+</template>
+
+<script>
+export default {
+  name: "Faq",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+@import '../mq'
+
+</style>
---
+++ compare-vue/src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">Home</div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+@import '../mq'
+
+</style>
