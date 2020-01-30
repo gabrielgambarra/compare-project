333806b88973dd7406f5ccc8fe8aa2f7a1aed60b
Gabriel Gambarra <gabriel.gambarra@itriad.org.br>
2020-01-30 16:47:41 -0400

    Clean structure and create other components

---
+++ compare-react/src/components/Faq/Faq.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// Stateless
+
+const Faq = () => {
+    return (
+        <div className="App">
+            Faq
+        </div>
+    );
+}
+
+export default Faq;
--- compare-react/src/components/Header/Header.js
+++ compare-react/src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+// Statefull
 
 class Header extends Component {
   render() {
